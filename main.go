2f5fc85a40e088a6fc7f007cbb6784ad8ee2d8a7
hodanov <1031hoda@gmail.com>
2020-01-04 11:43:51 +0900

    go test

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Calculate(x int) (result int) {
+	result = x + 3
+	return result
+}
+
+func main() {
+	result := Calculate(3)
+	fmt.Printf("3 + 2 = %v\n", result)
+}
