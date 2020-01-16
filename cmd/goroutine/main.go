6b43e943ed72536231e5bc863c5191c1f606d05b
hodanov <1031hoda@gmail.com>
2020-01-16 09:32:00 +0900

    goroutine

--- cmd/goroutine/main.go
+++ cmd/goroutine/main.go
@@ -2,44 +2,92 @@ package main
 
 import (
 	"fmt"
-	"sync"
+	// "sync"
 	//	"time"
 )
+
+/*
+fan-out fan-in
+
+Goroutine1(Producer)
+↓↓↓chan
+Goroutine2(Second stage)
+↓↓↓chan
+Goroutine3(Third stage)
+↓↓↓chan
+main()
+
+上記のように、goroutineで並列処理したものを出力（fanction-out)
+次のgoroutineへ渡して処理（fanction-in)　する的なやつ
+*/
+func producer(first chan int) {
+	defer close(first)
+	for i := 0; i < 10; i++ {
+		first <- i
+	}
+}
+
+func multi2(first <-chan int, second chan<- int) {
+	defer close(second)
+	for i := range first {
+		second <- i * 2
+	}
+}
+
+func multi4(second <-chan int, third chan<- int) {
+	defer close(third)
+	for i := range second {
+		third <- i * 2
+	}
+}
+
+func main() {
+	first := make(chan int)
+	second := make(chan int)
+	third := make(chan int)
+
+	go producer(first)
+	go multi2(first, second)
+	go multi4(second, third)
+	for result := range third {
+		fmt.Println(result)
+	}
+}
 
 /*
 producerとconsumer
 複数のサーバーのログを並列で取ってきて（producer）、
 さらに同時並行で何か処理をさせたい（consumer）とかとか。
 */
-func producer(ch chan int, i int) {
-	ch <- i * 2
-}
-
-func consumer(ch chan int, wg *sync.WaitGroup) {
-	for i := range ch {
-		func() {
-			defer wg.Done()
-			fmt.Println("process", i*1000)
-		}() // inner function
-	}
-}
-
-func main() {
-	var wg sync.WaitGroup
-	ch := make(chan int)
-
-	// Producer
-	for i := 0; i < 10; i++ {
-		wg.Add(1)
-		go producer(ch, i)
-	}
-
-	// Consumer
-	go consumer(ch, &wg)
-	wg.Wait()
-	close(ch)
-	fmt.Println("Done")
-}
+// func producer(ch chan int, i int) {
+// 	ch <- i * 2
+// }
+//
+// func consumer(ch chan int, wg *sync.WaitGroup) {
+// 	for i := range ch {
+// 		func() {
+// 			defer wg.Done()
+// 			fmt.Println("process", i*1000)
+// 		}() // inner function
+// 	}
+// }
+//
+// func main() {
+// 	var wg sync.WaitGroup
+// 	ch := make(chan int)
+//
+// 	// Producer
+// 	for i := 0; i < 10; i++ {
+// 		wg.Add(1)
+// 		go producer(ch, i)
+// 	}
+//
+// 	// Consumer
+// 	go consumer(ch, &wg)
+// 	wg.Wait()
+// 	close(ch)
+// 	fmt.Println("Done")
+// }
 
 /*
 channelとrangeとclose
