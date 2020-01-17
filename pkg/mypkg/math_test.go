d30d2c5c6a33a5f31267039e4d250b319102e668
hodanov <1031hoda@gmail.com>
2020-01-18 00:18:47 +0900

    Add Example

--- pkg/mypkg/math_test.go
+++ pkg/mypkg/math_test.go
@@ -1,24 +1,35 @@
 package mypkg
 
 import (
+	"fmt"
 	"testing"
 )
 
-func TestAverage(t *testing.T) {
+func Example() {
+	v := Sum([]int{1, 2, 3, 4, 5})
+	fmt.Println(v)
+}
+
+func ExampleSum() {
+	v := Sum([]int{1, 2, 3, 4, 5})
+	fmt.Println(v)
+}
+
+func TestSum(t *testing.T) {
 	tests := []struct {
 		name  string
 		input []int
 		want  int
 	}{
-		{"All positive integers", []int{1, 2, 3, 4, 5}, 3},
-		{"Partially negative integer", []int{-1, 2, 3, 4, 5}, 2},
-		{"Valid negative integer", []int{1, 2, 3, -4, -5}, 0},
-		{"All negative integers", []int{-1, -2, -3, -4, -5}, -3},
+		{"All positive integers", []int{1, 2, 3, 4, 5}, 15},
+		{"Partially negative integer", []int{-1, 2, 3, 4, 5}, 13},
+		{"Valid negative integer", []int{1, 2, 3, -4, -5}, -3},
+		{"All negative integers", []int{-1, -2, -3, -4, -5}, -15},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			output := Average(tt.input)
+			output := Sum(tt.input)
 			if output != tt.want {
 				t.Errorf("want %d; got %d; input %d", tt.want, output, tt.input)
 			}
