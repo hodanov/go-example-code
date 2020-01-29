6ed9347121439874488ce50ea3851acc08a59a4f
hodanov <1031hoda@gmail.com>
2020-01-29 12:50:56 +0900

    db-test

--- cmd/db-test/initdb.go
+++ cmd/db-test/initdb.go
@@ -3,24 +3,21 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"os"
+
+	_ "github.com/lib/pq"
 )
 
 func initDsn() string {
 	var dsn string
 
 	var (
-		// server   = os.Getenv("POSTGRES_SERVER")
-		// port     = os.Getenv("POSTGRES_PORT")
-		// user     = os.Getenv("POSTGRES_USER")
-		// password = os.Getenv("POSTGRES_PASSWORD")
-		// database = os.Getenv("POSTGRES_DB")
-		server   = "psql"
-		port     = "5432"
-		user     = "test"
-		password = "test"
-		database = "test"
+		server   = os.Getenv("POSTGRES_SERVER")
+		port     = os.Getenv("POSTGRES_PORT")
+		user     = os.Getenv("POSTGRES_USER")
+		password = os.Getenv("POSTGRES_PASSWORD")
+		database = os.Getenv("POSTGRES_DB")
 	)
-	// dsn = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", user, password, server, port, database)
 	dsn = fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", user, password, server, port, database)
 
 	return dsn
