355f3a1806b3c9161bacffc4df65486e1e85319e
hodanov <1031hoda@gmail.com>
2020-02-01 19:37:47 +0900

    db-test

--- cmd/db-test/main.go
+++ cmd/db-test/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"math/rand"
 )
@@ -14,17 +15,26 @@ func main() {
 	}
 	defer db.Close()
 
+	fmt.Println("Execute an insert statement.")
 	err = Insert(db, "hogehoge", rand.Intn(100))
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	fmt.Println("Query a multi select statement.")
 	err = MultiSelect(db)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	fmt.Println("Query a single select statement.")
 	err = SingleSelect(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Execute a delete statement.")
+	err = Delete(db)
 	if err != nil {
 		log.Fatal(err)
 	}
